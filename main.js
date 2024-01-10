8a59ffaf80e9dae871b2057108ec0960e1529a88
Bozo2023 <ulrob1.0@email.com>
2024-01-10 17:20:07 +0100

    enter

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 // Générer un nombre aléatoire entre 1 et 6 pour le joueur et l'ordinateur
 let giocatore = Math.floor(Math.random() * 6) + 1;
 let computer = Math.floor(Math.random() * 6) + 1;
-
+let listEmail=["didier@yahoo.fr,rossi@hotmail.com,ulrich@gmail.com,ted@yahooo.it,eric@rokmail.com"];
+const emailHtml=document.getElementById("email")
+const ResultHtml=document.getElementById("enter")
 // Déterminer le gagnant en fonction du nombre le plus élevé
 if (giocatore > computer) {
   console.log("il vincitore è il giocatore con  " + giocatore+ " !");
@@ -11,3 +13,9 @@ if (giocatore > computer) {
 } else {
   console.log("parità !");
 }
+ResultHtml.addEventListener("click",function(){
+    
+})
+function controllaEmail(email) {
+    return listaEmail.includes(email);
+  }
