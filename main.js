6f4669556ac5c22f1cc85b978565ff456eaa28cf
Bozo2023 <ulrob1.0@email.com>
2024-01-11 00:34:07 +0100

    yes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // Générer un nombre aléatoire entre 1 et 6 pour le joueur et l'ordinateur
 let giocatore = Math.floor(Math.random() * 6) + 1;
 let computer = Math.floor(Math.random() * 6) + 1;
-let listEmail=["didier@yahoo.fr,rossi@hotmail.com,ulrich@gmail.com,ted@yahooo.it,eric@rokmail.com"];
+let listaEmail=["didier@yahoo.fr,rossi@hotmail.com,ulrich@gmail.com,ted@yahooo.it,eric@rokmail.com"];
 const emailHtml=document.getElementById("email")
 const ResultHtml=document.getElementById("enter")
 // Déterminer le gagnant en fonction du nombre le plus élevé
@@ -14,7 +14,16 @@ if (giocatore > computer) {
   console.log("parità !");
 }
 ResultHtml.addEventListener("click",function(){
-    
+    let emailUtente = document.getElementById("email");
+    if (controllaEmail(emailUtente)) {
+      alert("L'utente è autorizzato ad accedere.");
+      console.log("L'utente è autorizzato ad accedere.");
+
+    } else {
+      alert("L'utente non è autorizzato ad accedere.");
+      console.log("L'utente non è autorizzato ad accedere.");
+
+    }
 })
 function controllaEmail(email) {
     return listaEmail.includes(email);
